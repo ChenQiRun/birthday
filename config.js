8c615a19c9af31115ddb1760ceda5bd4707ef302
ChenQiRun <78997144+ChenQiRun@users.noreply.github.com>
2021-02-13 16:45:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 // QIRUN 2021.2.13 https://github.com/AJLoveChina/birthday
- config = {
+var config = {
     texts: [        
         "迪士尼在逃公主👸",
        "02.13",
-        "今天是你的生日",
+        "今天是👸的生日",
         "今天也很 迪丽热巴 wo wo(夸人的最高境界hahahha)",
         "👸还是要长大，可爱不是长久之计",
         "岁岁年年，万事万般宜",
     ],
     imgs: {
         "迪士尼在逃公主👸": "./imgs/mmexport1613196764740.jpg",
-        "今天也很 迪丽热巴 wo wo(夸人的最高境界hahahha) ": "./imgs/IMG_20210213_151634.jpg",
+        "今天也很 迪丽热巴 wo wo(夸人的最高境界hahahha)": "./imgs/IMG_20210213_151634.jpg",
     },
     desc: {
         turn_on: "click",
